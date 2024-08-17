9f104476ea66658096fd1ffd5bcfe7bb9de71190
mgsoumya <90186413+mgsoumya@users.noreply.github.com>
2024-08-17 13:00:22 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,18 @@
+package starPattern;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// prints on same line
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		//prints on new line
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+	}
+
+}
